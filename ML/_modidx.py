ba4ee6ac5e73ea26dc74c3e56ef602f6a2d53b73
Benedict Thekkel <bthekkel1@gmail.com>
2023-07-20 18:21:07 +1000

    update1

--- ML/_modidx.py
+++ ML/_modidx.py
@@ -5,4 +5,4 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://bthek1.github.io',
                 'git_url': 'https://github.com/bthek1/ML',
                 'lib_path': 'ML'},
-  'syms': {'ML.core': {'ML.core.foo': ('core.html#foo', 'ML/core.py')}}}
+  'syms': {'ML.core': {'ML.core.foo': ('core.html#foo', 'ML/core.py')}, 'ML.knn': {'ML.knn.sayhello': ('k-nn.html#sayhello', 'ML/knn.py')}}}
